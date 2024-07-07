e7ca5961df1831b68a0b0dd18a3f93d3d291c42e
Satya <142731044+Satya9121@users.noreply.github.com>
2024-07-07 16:36:58 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Title ="Average";
+
+int boys;
+int girls;                                                       //integers are must be denoted
+int teachers;
+
+Console.Write("Total number of boys:");
+boys = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Total number of girls:");
+girls = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Total number of teachers:");
+teachers = Convert.ToInt32(Console.ReadLine());
+
+int average = (boys + girls + teachers)/3;                      //Anything would be calculated in the form of result or something
+Console.WriteLine("You're class average is:" + average);        //State must be denoted 
+
+Console.ReadKey();
